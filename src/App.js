cc9a6741b87874d76f57dda2b806b9d92e6c3f09
Jessa <67758781+jes-sun@users.noreply.github.com>
2021-06-03 13:20:34 -0400

    Purged v1 React components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,20 @@
-import React from 'react';
-import './css/App.css';
-import Navbar from './Navbar';
-import Homepage from './Homepage';
-import Login from './Login';
-import Register from './Register';
-import Group from './Group';
-import Member from './Member';
-import Search from './Search';
-import Profile from './Profile';
-import Edit from './Edit';
-import MyGroup from './MyGroup';
-import PerfOptions from './PerfOptions';
-import Performance from './Performance';
-import Leaderboard from './Leaderboard';
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import { Route, Switch, Redirect } from 'react-router-dom';
+import Homepage from "./Homepage";
 
-class App extends React.Component {
-    loggedIn() {
-        if(sessionStorage.getItem("login")) {
-            return true;
-        } else return false;
+function App() {
+    function isloggedIn() {
+        return sessionStorage.getItem("login") ? true : false;
     }
 
-    render() {
-        return (
-            <main>
-                <Navbar/>
-                <Switch> 
-                    <Route exact path="/">
-                        <Redirect to="/home"/>
-                    </Route>
-                    <Route path="/home" component={Homepage} exact />
-                    <Route path="/login" component={Login} exact />
-                    <Route path="/register" component={Register} exact />                                          
-                    <Route exact path="/profile">
-                        {this.loggedIn() ? <Redirect to={"/profile/"+sessionStorage.getItem("login")}/> : <Redirect to="/login"/>}
-                    </Route>
-                    <Route exact path="/mygroup">
-                        {this.loggedIn() ? <Redirect to={"/"+sessionStorage.getItem("login")+"/mygroup"}/> : <Redirect to="/login"/>}
-                    </Route>
-                    <Route path="/profile/:username" component={Profile} exact />   
-                    <Route path="/:username/mygroup" component={MyGroup} exact />
-                    <Route path="/edit" component={Edit} exact />                
-                    <Route path="/groups/" component={Search} exact/> 
-                    <Route path="/groups/:group_name" component={Group} exact/> 
-                    <Route path="/groups/:group/members/:member" component={Member} exact/>
-                    <Route path="/perform" component={PerfOptions} exact />
-                    <Route path="/perform/go" component={Performance} exact />
-                    <Route path="/leaderboard" component={Leaderboard} exact />
-                </Switch>
-            </main>
-        )
-    }
+    return (
+        <main>
+            <Switch> 
+                <Route exact path="/" component={Homepage}/>
+            </Switch>
+        </main>
+    )
+    
 }
 
 export default App;
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,41 +1,9 @@
-import React from 'react';
-import './css/Main.css';
-import logo_full from './images/logo_full.png';
-import { Link } from 'react-router-dom';
-
-class Homepage extends React.Component {
-
-  render () { 
+function Homepage() {
     return (
-    <div className="Main">
-        <img className="logoFull" src={logo_full} height="100px"></img>
-        <iframe className="ytplayer"
-        id="LBytplayer" 
-        type="text/html" 
-        width="720" height="405"
-        src={"https://www.youtube.com/embed/M3rg-rh6MPo?autoplay=1&modestbranding=1&color=white"}
-        frameBorder="0" allowFullScreen>
-        </iframe>
-        <h1 className="idolName">Build your group!</h1>
-        <div className="homepageText">
-        <p>
-            Stop arguing with people online. Simply prove you're objectively better at being a K-pop fan by beating them at this game.
-        </p>
-        <ol id="homepageList">
-            <li className="homepageListItem">Select your favourite idols</li>
-            <li className="homepageListItem">Choose a song to cover</li>
-            <li className="homepageListItem"><Link to="/mygroup" className="slink">Perform!</Link></li>
-        </ol>
-        <p>
-            The #1 song on the leaderboard gets automatic YouTube Player privileges!
-        </p>
-        <p>
-            And bragging rights, I guess, if you think that would be an accomplishment worth bragging about.
-        </p>
-        </div>
-    </div>
-    );
-  };
+        <>
+        And so it begins.
+        </>
+    )
 }
 
 export default Homepage;
